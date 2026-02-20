3a6b569e9ad42dcb14b9878241a1e6903d03f902
intalred <soluinfolink@gmail.com>
2026-02-20 11:38:58 -0500

    Delete app directory

--- app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./globals.css";
-export const metadata = { title: "CCTV San Martín - ISO", description: "Gestión CCTV certificable" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (<html lang="es"><body>{children}</body></html>);
-}
