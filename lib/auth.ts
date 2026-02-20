a21702dd01636e906e9f0b042a9a84206c00e20e
intalred <soluinfolink@gmail.com>
2026-02-20 11:40:27 -0500

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,5 +0,0 @@
-export function usernameToEmail(username: string) {
-  const domain = process.env.NEXT_PUBLIC_USERNAME_EMAIL_DOMAIN || "sanmartin.local";
-  const u = (username || "").trim().toLowerCase();
-  return u ? `${u}@${domain}` : "";
-}
